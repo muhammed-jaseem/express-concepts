4662fa3d7e1dee3cb7fbb54b8f27cd360849356f
Muhammed Jaseem <muhammed.jaseem@trenser.com>
2026-08-26 15:00:02 +0530

    Add api versioning

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const {
   asyncHandler,
   globalErrorHandler,
 } = require("./middleware/errorHandler");
+const { urlVersioning } = require("./middleware/apiVersioning");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +20,8 @@ app.use(addTimeStamp);
 
 app.use(configureCors());
 app.use(express.json());
+
+app.use('/api/v1',urlVersioning('v1'))
 
 app.use(globalErrorHandler);
 
